b03dcaa1c572cf7b60287d5f466fc8c78cdd82fd
Samuel Duursma <sd.duursma@gmail.com>
2018-07-27 23:07:43 +0100

    Use deps.edn in combination with Leiningen assisted by lein-tools-deps
    To be able to grab dependencies directly from git repos.
    
    * Add basic git cloning mechanism.

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [irresponsible/tentacles "0.6.2"]
-                 [org.clojure/tools.logging "0.4.1"]]
+  :plugins [[lein-tools-deps "0.4.1"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
   :main ^:skip-aot github-merge-bot.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/github_merge_bot/core.clj
+++ src/github_merge_bot/core.clj
@@ -1,7 +1,9 @@
 (ns github-merge-bot.core
   (:require [tentacles.pulls :as pulls]
             [clojure.pprint :as pprint]
-            [clojure.tools.logging :as log])
+            [clojure.tools.logging :as log]
+            [clj-jgit.porcelain :as git])
+  (:import (java.util UUID))
   (:gen-class))
 
 (defn mergeable? [pull-id]
@@ -17,8 +19,17 @@
   (oldest (filter #(mergeable? (:number %))
                   pulls)))
 
+(defn rebase [])
+
+(defn update-pull [owner repo pull-id]
+  ;; TODO: Clone every time?
+  (git/git-clone-full (str "https://github.com/" owner "/" repo ".git")
+                      (str "./tmp/" (UUID/randomUUID) owner "/" repo)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
-  (pprint/pprint (:number (pull-to-update (pulls/pulls "sdduursma" "github-merge-bot-test")))))
+  (let [pull (:number (pull-to-update (pulls/pulls "sdduursma" "github-merge-bot-test")))]
+    (println pull)
+    (update-pull "sdduursma" "github-merge-bot-test" pull)))
