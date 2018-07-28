6ef0d6824a829bed6fd9afda707951d8c3b39e7e
Samuel Duursma <sd.duursma@gmail.com>
2018-07-28 19:50:42 +0100

    Pass credentials in via env vars

--- src/github_merge_bot/core.clj
+++ src/github_merge_bot/core.clj
@@ -20,7 +20,7 @@
   [(oldest (filter #(mergeable? (:number %))
                   pulls))])
 
-(defn update-pull [owner repo pull-request]
+(defn update-pull [owner repo pull-request credentials]
   ;; TODO: Clone every time?
   (let [repo (:repo (git/git-clone-full (str "https://github.com/" owner "/" repo ".git")
                                         (str "./tmp/" (UUID/randomUUID) owner "/" repo)))
@@ -34,12 +34,14 @@
         (.push)
         (.setRemote "origin")
         (.setForce true)
-        (.setCredentialsProvider (UsernamePasswordCredentialsProvider. "sdduursma" "xxx"))
+        (.setCredentialsProvider (UsernamePasswordCredentialsProvider. (:username credentials) (:password credentials)))
         (.call))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [pull-requests (pull-requests-to-update (pulls/pulls "sdduursma" "github-merge-bot-test"))]
+  (let [pull-requests (pull-requests-to-update (pulls/pulls "sdduursma" "github-merge-bot-test"))
+        credentials {:username (System/getenv "GITHUB_MERGE_BOT_USERNAME")
+                     :password (System/getenv "GITHUB_MERGE_BOT_PASSWORD")}]
     (doseq [pr pull-requests]
-      (update-pull "sdduursma" "github-merge-bot-test" pr))))
+      (update-pull "sdduursma" "github-merge-bot-test" pr credentials))))
