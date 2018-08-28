97c603af7fe927e4cd0f7de19f02b0c468719a4b
Samuel Duursma <sd.duursma@gmail.com>
2018-08-28 19:36:33 +0200

    Add test-update-pull-request

--- test/github_merge_bot/core_test.clj
+++ test/github_merge_bot/core_test.clj
@@ -1,11 +1,24 @@
 (ns github-merge-bot.core-test
   (:require [clojure.test :refer :all]
             [github-merge-bot.core :refer :all]
-            [tentacles.pulls :as pulls]))
+            [clj-jgit.porcelain :as git])
+  (:import (java.util UUID)
+           (org.eclipse.jgit.api Git)))
 
-(deftest test-head-up-to-date-with-base
-  (testing
-    (let [pr (pulls/specific-pull "sdduursma"  "github-merge-bot-test" 18)]
-      (is (not (head-up-to-date-with-base? "sdduursma" "github-merge-bot-test" pr))))
-    (let [pr (pulls/specific-pull "sdduursma" "github-merge-bot-test" 19)]
-      (is (head-up-to-date-with-base? "sdduursma" "github-merge-bot-test" pr)))))
+(defn pull-request-for-repo-branch [^Git origin-repo branch-name]
+  {:number 1
+   :head {:sha (.getName (.getObjectId (.findRef (.getRepository origin-repo) branch-name)))
+          :ref branch-name}})
+
+(deftest test-update-pull-request
+  (let [origin-repo-dir (str (System/getProperty "java.io.tmpdir") (UUID/randomUUID))
+        origin-repo (git/git-init origin-repo-dir)
+        rev-a (git/git-commit origin-repo "A")
+        _ (git/git-commit origin-repo "B")
+        _ (git/git-checkout origin-repo "feature-c" true false (.getName (.getId rev-a)))
+        _ (git/git-commit origin-repo "C")
+        _ (git/git-checkout origin-repo "master")
+        repo (:repo (git/git-clone-full origin-repo-dir (str (System/getProperty "java.io.tmpdir") (UUID/randomUUID))))]
+    (with-redefs [procure-repo (constantly repo)]
+      (update-pull "foo" "bar" (pull-request-for-repo-branch origin-repo "feature-c") {:username "foo" :password "abc"})
+      (is (head-up-to-date-with-base? "foo" "bar" (pull-request-for-repo-branch origin-repo "feature-c"))))))
